2497699f9ea507a417f8910d0192fb847274d6ee
Freefallsofme <hurtains@gmail.com>
2025-06-10 20:09:27 +0500

    Add dain.tf Terraform configuration to task_1 branch

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket         = "frfllsbucket"
+    key            = "terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+#resource "aws_s3_bucket" "state" {
+ # bucket = "frfllsbucket"
+  #acl    = "private"
+
+  #versioning {
+   # enabled = true
+ # }
+#}
+
+#resource "aws_dynamodb_table" "locks" {
+  #name         = "terraform-lock-table"
+  #billing_mode = "PAY_PER_REQUEST"
+  #hash_key     = "LockID"
+
+  #attribute {
+   # name = "LockID"
+    #type = "S"
+ #}
+#}
+
