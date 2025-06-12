0a1a11f147cb49fe17a0155485b87fcfbf3346d1
Freefallsofme <hurtains@gmail.com>
2025-06-13 01:05:11 +0500

    Changed tf file. Separated it to 3 different and became a happy suka

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket         = "frfllsbucket"
+    key            = "terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
+}
---
+++ bucket.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "state_pls" {
+  bucket = "allosukadevopsvpops"
+ # acl    = "private"
+
+ # versioning {
+  #  enabled = true
+  #}
+}
+
