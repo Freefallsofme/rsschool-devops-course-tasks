c33a0bce6c3f6397480c827a59a31638b4bb5e93
Freefallsofme <hurtains@gmail.com>
2025-06-18 00:49:41 +0500

    terraform ssec gropus conf upd

---
+++ security-groups.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastionsg"
+  description = "Allow SSH access from my IP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["45.85.105.205/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Bastion"
+  }
+}
+
+resource "aws_security_group" "private_sg" {
+  name        = "privatesg"
+  description = "Allow SSH from bastion host"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "SSH from Bastion SG"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["45.85.105.205/32"]
+  }
+
+  tags = {
+    Name = "Private"
+  }
+}
