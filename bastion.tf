de0d26efa8e42fdf2d9ca48784ee867a49d39e98
Freefallsofme <hurtains@gmail.com>
2025-06-27 02:06:44 +0500

    smthng

--- bastion.tf
+++ bastion.tf
@@ -14,12 +14,17 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKJu+I0h2RSDmUicNvUJ2b6HphZmp5aAD2SwUh7Bwrfs hurtains@gmail.com"
+}
+
 resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   associate_public_ip_address = true
-
+  key_name      = aws_key_pair.deployer.key_name
   tags = { Name = "BastionHost" }
 }
