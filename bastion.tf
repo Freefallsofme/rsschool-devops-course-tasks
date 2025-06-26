bf9d1e873958189cf6d9124eecb0d2b16203ed3c
Freefallsofme <hurtains@gmail.com>
2025-06-27 02:08:12 +0500

    smthng

--- bastion.tf
+++ bastion.tf
@@ -25,6 +25,6 @@ resource "aws_instance" "bastion" {
   subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   associate_public_ip_address = true
-  key_name      = aws_key_pair.deployer.key_name
-  tags = { Name = "BastionHost" }
+  key_name                    = aws_key_pair.deployer.key_name
+  tags                        = { Name = "BastionHost" }
 }
