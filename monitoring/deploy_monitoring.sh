1908485ada00b4824c739a4ce9c3326fb333dc81
Freefallsofme <hurtains@gmail.com>
2025-07-29 16:26:56 +0500

    adding dashboard, checking script for deploy, remaking alert rules

--- monitoring/deploy_monitoring.sh
+++ monitoring/deploy_monitoring.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-# Проверяем и создаем namespace, если он не существует
+# Checking or creating namespace
 kubectl get namespace monitoring > /dev/null 2>&1 || kubectl create namespace monitoring
 
-# Добавляем Helm-репозитории
+# Adding Helm-repo
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
-# Устанавливаем или обновляем Prometheus
+# Install or updating Prometheus
 helm upgrade --install prometheus prometheus-community/prometheus -n monitoring -f prometheusval.yaml
 
-# Разворачиваем smtp4dev
+# Deploying smtp4dev
 kubectl apply -f smtp4dev.yaml
 
-# Создаем секрет для админа Grafana, если он не существует
+# Creating secret for Grafana's admin
 kubectl get secret grafana-env -n monitoring > /dev/null 2>&1 || kubectl create secret generic grafana-env --from-literal=GF_SECURITY_ADMIN_PASSWORD=mysecretpassword -n monitoring
 
-# Создаем ConfigMap для provisioning Grafana
+# Creating ConfigMap for provisioning Grafana
 kubectl create configmap grafana-provisioning \
   --from-file=rules.yaml=grafana-provisioning/alerting/rules.yaml \
   --from-file=contactpoints.yaml=grafana-provisioning/notifications/contactpoints.yaml \
   --from-file=policies.yaml=grafana-provisioning/notifications/policies.yaml \
   -n monitoring
 
-# Устанавливаем или обновляем Grafana
+# Creating ConfigMap for dashboard
+kubectl create configmap grafana-dashboards \
+  --from-file=custom-dashboard.json=grafana-provisioning/dashboards/custom-dashboard.json \
+  -n monitoring
+
+# Install or updating Grafana
 helm upgrade --install grafana grafana/grafana -n monitoring -f grafanaval.yaml
